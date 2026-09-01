c5e75b5badb07dddde0883e0635d512ebc263b61
gptpaulovitor-sudo <gptpaulovitor@gmail.com>
2026-09-01 09:08:26 -0300

    deploy: publicar versao de producao com cartao whatsapp e bump no service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Solus Christus PWA Service Worker
-const CACHE_NAME = 'solus-christus-v1';
+const CACHE_NAME = 'solus-christus-v3';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Solus Christus PWA Service Worker
-const CACHE_NAME = 'solus-christus-v2';
+const CACHE_NAME = 'solus-christus-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
